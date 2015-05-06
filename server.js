9a0ec86dff5e0c59797f0b571efb0443f6239f95
ZeZeN <bencrdc@gmail.com>
2015-05-07 00:37:18 +0200

    randomized articles render + some layout customisations

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-
+var fs      = require('fs');
 var express = require('express');
 
 ////////////////////
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public/'));
 
 
 //==========================Les routes============================//
-// /!\Warning, The interface route must always be the last one (because it takes care of everything that have not been already routed...) /!\
+// /!\ Attention, l'interface doit toujours etre la derniere route! /!\
 
 
 /////////
@@ -19,10 +19,24 @@ app.use(express.static(__dirname + '/public/'));
 /////////
 
 app.get('/api/articles', function(res, res){
-	res.sendFile('data.json', {"root": __dirname + '/server'});
+	// res.sendFile('data.json', {"root": __dirname + '/server'});
+	
+	var content,
+		filepath = __dirname + '/server/data.json',
+	//get the data
+		content  = JSON.parse(fs.readFileSync(filepath,'utf8'));
+
+	//random article from the articlesData array :)  
+	//(just to be sure I can do something with the data...)
+	var tmp = Math.floor((Math.random() * content.articlesData.length)/* + 1*/);
+
+	content.articlesData = [content.articlesData[tmp]];
+
+	//send 	
+	res.setHeader('Content-Type', 'application/json');
+	res.end(JSON.stringify(content));
+
 })
-
-
 
 
 
@@ -31,18 +45,18 @@ app.get('/api/articles', function(res, res){
 ///////////////
 
 app.use(function(req, res) {
-  return res.redirect(req.protocol + '://' + req.get('Host') + '/#' + req.url)
+
+	return res.redirect(req.protocol + '://' + req.get('Host') + '/#' + req.url)
+
 })
-
-
 
 
 
 //=======================DEMARRAGE du serveur==========================//
 
 var address = '127.0.0.1',
-	port = '1234',
-	full = 'http://'+address+':'+port;
+	port    = '1234',
+	full    = 'http://'+address+':'+port;
 
 app.listen(port);
 console.log('server running at : '+full)
